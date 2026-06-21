8da3166eb795a844e4adfd096e414b078ea9cb00
joaovlummertz <joaolummertz1@gmail.com>
2026-06-21 15:11:05 -0300

    improved collision resolution by creating a collision scene

---
+++ include/SceneData.h
@@ -0,0 +1,35 @@
+#ifndef SCENE_DATA_H
+#define SCENE_DATA_H
+
+#include <cstddef>
+#include <map>
+#include <string>
+#include <vector>
+
+#include <glad/glad.h>
+#include <glm/vec3.hpp>
+
+struct SceneObject
+{
+    std::string name;
+    size_t first_index;
+    size_t num_indices;
+    GLenum rendering_mode;
+    GLuint vertex_array_object_id;
+    glm::vec3 bbox_min;
+    glm::vec3 bbox_max;
+    GLuint texture_id;
+};
+
+struct CollisionObject
+{
+    std::string name;
+    glm::vec3 bbox_min;
+    glm::vec3 bbox_max;
+    std::vector<glm::vec3> vertices;
+};
+
+extern std::map<std::string, SceneObject> g_VirtualScene;
+extern std::vector<CollisionObject> g_CollisionScene;
+
+#endif
