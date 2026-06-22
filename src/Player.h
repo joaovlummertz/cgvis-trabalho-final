bfdbf38eb621e86138d9cfe7ca45021ad8ea6d25
joaovlummertz <joaolummertz1@gmail.com>
2026-06-22 02:54:13 -0300

    improve gravity on ramps PROMPT: gravity is not working well on slopes, what are the possible causes?

--- src/Player.h
+++ src/Player.h
@@ -2,15 +2,17 @@
 #define PLAYER_H
 
 #include <glm/vec4.hpp>
+#include <glm/vec3.hpp>
 
 struct PlayerState
 {
     glm::vec4 g_PlayerPosition = glm::vec4(31.247f, -4.820f, -6.341f, 0.0f);
-    float g_PlayerMoveSpeed = 8.0f;
+    float g_PlayerMoveSpeed = 6.5f;
     float g_PlayerYaw = 0.0f;
     float g_PlayerPitch = 0.0f;
     float g_PlayerVerticalVelocity = 0.0f;
     bool g_PlayerIsGrounded = false;
+    glm::vec3 g_PlayerGroundNormal = glm::vec3(0.0f, 1.0f, 0.0f);
 };
 namespace Player
 {
